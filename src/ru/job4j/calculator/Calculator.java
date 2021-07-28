7ef0b1dea0819c2ddafe708ac339958f394a7595
KirillFor <job4jtalladi71@gmail.com>
2021-07-28 07:50:52 +0500

    4.1. Аргументы [#295521]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,13 @@
 package ru.job4j.calculator;
 
-public class ArgMethod {
-
-    public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
-    }
-
+public class Calculator {
     public static void main(String[] args) {
-        String name = "Job4j";
-        int age = 33;
-        ArgMethod.hello(name, age);
-        ArgMethod.hello(name, age);
-        ArgMethod.hello(name, age);
-        ArgMethod.hello(name, age);
+      int size = 10;
+        System.out.println(size);
+        size = 100;
+        System.out.println(size);
+        size = size -5;
+        System.out.println(size);
+
     }
 }
